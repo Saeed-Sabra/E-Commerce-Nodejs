e8c0dabf91cf633b1bcd96f771e01affc393b7ab
Saeed Sabra <saeedsabra565@gmail.com>
2023-09-11 23:10:17 +0300

    finish jwt and user APIS

--- src/routers/users.js
+++ src/routers/users.js
@@ -16,7 +16,7 @@ router.get("/", async (req, res) => {
   res.status(200).send(users);
 });
 
-router.post("/signup", async (req, res) => {
+router.post("/register", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
@@ -47,15 +47,43 @@ router.post("/login", async (req, res) => {
     }
     if (user && bcrypt.compareSync(req.body.password, user.password)) {
       console.log(process.env.JWT_SECRET);
-      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-        expiresIn: "1d",
-      });
+      const token = jwt.sign(
+        { userId: user.id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "1d",
+        }
+      );
       res.send({ user, token });
     } else {
       res.status(400).send("password is wrong!");
     }
   } catch (error) {
     res.status(400).send();
+  }
+});
+
+router.get("/get/count", async (req, res) => {
+  const usersCount = await User.countDocuments();
+
+  if (!usersCount) {
+    res.status(500).json({ success: false });
+  }
+  res.send({
+    usersCount,
+    success: true,
+  });
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndRemove(req.params.id);
+    if (!user) {
+      return res.status(409).send("no such user");
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
   }
 });
 module.exports = router;
